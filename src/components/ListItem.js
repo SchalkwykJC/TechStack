3dccbf2f31af87952914fedbe9a57c250de08e6b
jaco <jaco@DESKTOP-UK88DE0>
2017-08-08 04:09:11 +0200

    Library List

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,3 +1,15 @@
 import React, {Component} from 'react';
+import {Text} from 'react-native';
+import {CardSection} from './common';
 
-class ListItem extends Component
+class ListItem extends Component {
+  render() {
+    return (
+        <CardSection>
+          <Text>{this.props.library.title}</Text>
+        </CardSection>
+    );
+  }
+}
+
+export default ListItem;
